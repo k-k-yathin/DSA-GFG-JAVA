b5993d110e531efa712f1e66a28d32d5fdbb4b94
k-k-yathin <yathinkarnamkiran@gmail.com>
2025-12-18 22:15:38 +0530

    Create MaxProdSubArray.java

---
+++ Arrays/MaxProdSubArray.java
@@ -0,0 +1,24 @@
+public class MaxProdSubArray {
+    int maxProduct(int[] arr) {
+        int maxEnding = arr[0];
+        int minEnding = arr[0];
+        int maxSoFar = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            int temp = maxEnding;
+
+            maxEnding = Math.max(arr[i],
+                    Math.max(maxEnding * arr[i], minEnding * arr[i]));
+
+            minEnding = Math.min(arr[i],
+                    Math.min(temp * arr[i], minEnding * arr[i]));
+
+            maxSoFar = Math.max(maxSoFar, maxEnding);
+        }
+        return maxSoFar;
+    }
+    public static void main(String[] args) {
+        MaxProdSubArray g = new MaxProdSubArray();
+        System.out.println(g.maxProduct(new int[]{1,2,3,4,-7,7,8,9,10}));
+    }
+}
