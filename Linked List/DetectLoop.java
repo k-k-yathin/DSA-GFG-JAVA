c47d95a6e558abb73de18514aeede502e51f37fa
K K YATHIN <yathinkarnamkiran@gmail.com>
2026-02-15 00:08:07 +0530

    Implement loop detection in linked list

---
+++ Linked List/DetectLoop.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean detectLoop(Node head) {
+        
+        if(head==null || head.next ==null) return false;
+        Node fast = head ;
+        Node slow = head ;
+        while(fast !=null && fast.next!= null ){
+            slow = slow.next;
+            fast=fast.next.next;
+            
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
