c7d5fb318be2957fed5c6e317a3ce5fa73d73626
K K YATHIN <yathinkarnamkiran@gmail.com>
2026-02-20 23:38:41 +0530

    feat: Implement LRU cache using LinkedHashMap
    The LRUCache class utilizes a LinkedHashMap to maintain the order of entries based on access, ensuring efficient eviction of the least recently used items when capacity is exceeded.

---
+++ Linked List/LRUCache.java
@@ -0,0 +1,22 @@
+class LRUCache {
+    private int capacity;
+    private LinkedHashMap<Integer , Integer> cache;
+    
+    LRUCache(int cap) {
+        this.capacity = cap;
+        this.cache = new LinkedHashMap<>(cap, 0.75f, true) {
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+        
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+}
