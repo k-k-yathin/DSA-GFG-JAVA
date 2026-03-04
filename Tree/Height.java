75ef3c66df40b94780c332c01000f0727d33ded9
K K YATHIN <yathinkarnamkiran@gmail.com>
2026-03-04 16:33:29 +0530

    Add height calculation method for binary tree

---
+++ Tree/Height.java
@@ -0,0 +1,10 @@
+class Solution {
+    int height(Node root) {
+        if (root == null) return -1;
+        
+        int left = height(root.left);
+        int right = height(root.right);
+        
+        return Math.max(left, right) + 1;
+    }
+}
