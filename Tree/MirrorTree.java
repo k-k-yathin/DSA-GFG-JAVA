9066c134b5fe3dbd570a9b0e5cd69fb34a533db6
K K YATHIN <yathinkarnamkiran@gmail.com>
2026-03-05 23:28:50 +0530

    Add mirror function to invert binary tree

---
+++ Tree/MirrorTree.java
@@ -0,0 +1,13 @@
+class Solution {
+    void mirror(Node root) {
+        
+        if(root == null) return;
+        
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        mirror(root.left);
+        mirror(root.right);
+    }
+}
