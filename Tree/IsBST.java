96e24b408370e047bf43db5f2508ce4ee7ff7b39
K K YATHIN <yathinkarnamkiran@gmail.com>
2026-03-09 14:19:58 +0530

    check binary search tree

---
+++ Tree/IsBST.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isBST(Node root) {
+        return check(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    boolean check(Node node, long min, long max){
+        if(node == null) return true;
+        
+        if(node.data <= min || node.data >= max)
+            return false;
+            
+        return check(node.left, min, node.data) &&
+               check(node.right, node.data, max);
+    }
+}
